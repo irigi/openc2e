5ce5acaf9cd6b566f3ffe639c689092bcc5e4ebb
irigi <irigi@matfyz.cz>
2013-03-16 20:11:07 -0400

    epic walking cursor according to http://www.paulgriffiths.net/program/c/curses.php , after hours spent on installing curses lib, this is finally worth it :)

--- src/TextWorld.h
+++ src/TextWorld.h
@@ -8,9 +8,26 @@
 #ifndef TEXTWORLD_H_
 #define TEXTWORLD_H_
 
+#include <ncurses.h>
+
 void startCurses();
 void endCurses();
 void text_world();
+
+// this should really be a singleton class, but I don't know how to do that now
+// so I'm keeping it like this
+class textWindow {
+public:
+	textWindow();
+	~textWindow();
+	void initColors();
+	static char * intprtkey(int ch);
+
+private:
+	WINDOW * mainwin;
+	int oldcur;
+
+};
 
 
 #endif /* TEXTWORLD_H_ */
